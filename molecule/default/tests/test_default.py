13e491558d8d783194e70b81d8e8a796945de2b7
Pablo Muñoz <pablerass@gmail.com>
2018-09-06 19:05:56 +0200

    Initial commit with basic tests

---
+++ molecule/default/tests/test_default.py
@@ -0,0 +1,20 @@
+"""Defautl test module."""
+import os
+
+import testinfra.utils.ansible_runner
+
+testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
+    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
+
+
+def test_service(host):
+    """Test service."""
+    service = host.service("grafana")
+    assert service.is_enabled
+    assert service.is_running
+
+
+def test_socket(host):
+    """Test socket connection."""
+    socket = host.socket("tcp://0.0.0.0:3000")
+    assert socket.is_listening
